3e42e4d703066a4a417a16c683df16c8fde3fb63
Parth Patel <67832224+Part-h@users.noreply.github.com>
2024-02-26 13:10:59 -0500

    files creation and setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import Home from "./Views/Home/HomeScreen/index.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React in seconds
-        </a>
-      </header>
+        <Home/>
     </div>
   );
 }
 
 export default App;
+// or a function can later be exported using export default App
---
+++ src/Views/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection(){
+    return (
+        <section id="herosection" className="hero--section">
+            <div className="hero--section--content--box">
+                <div className="hero--section-content">
+                    <p className="section--title">Hey, I'm Parth</p>
+                    <h1 className="hero--section--title">
+                        <span className="hero--section-title--color">Full Stack</span>{" "}
+                    <br/>
+                    Developer
+                    </h1>
+                    <p className="hero--section-description">
+                            Lorem asdnasjkdnasnd askkdaksmdkasndas akmdkasda.
+                        <br/> asdnmakkmdkam, asdokasodkmsdakmsa'.asdmalmdapmd
+                    </p>
+                </div>
+                <button className="btn btn-primary">Get in touch</button>
+            </div>
+            <div className="hero--section--img">
+                <img src="./img/hero_img.png" alt="Hero Section"></img>
+            </div>
+        </section>
+    )
+}
