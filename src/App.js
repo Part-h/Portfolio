6ee4cfefb60f817ef441941fa238eeaf65684e21
Parth Patel <67832224+Part-h@users.noreply.github.com>
2024-02-28 01:36:27 -0500

    Navbar configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import Navbar from './Views/Home/Navbar.jsx';
 import Home from "./Views/Home/HomeScreen/index.jsx";
+
 
 function App() {
   return (
     <div className="App">
-        <Home/>
+        <Router>
+          <div>
+            <Navbar/>
+            <Routes>
+              <Route path="/" element={<Home/>}></Route>
+              <Route path="*" element={<div> 404 page not found</div>}></Route>
+              <Route path="/" element={<Home/>}></Route>
+              <Route path="/" element={<Home/>}></Route>
+            
+            </Routes>
+          </div>
+        </Router>
     </div>
   );
 }
--- src/Views/Home/Navbar.jsx
+++ src/Views/Home/Navbar.jsx
@@ -1,18 +1,46 @@
 import { useState, useEffect } from "react";
-import {Link} from "react-scroll";
+import { Link } from "react-scroll";
 
-function Navbar(){
-    //useState is a hook to manage the state of variable 'NavActive'. 
-    //The useState hook returns an array with two elements: the current state value (NavActive) and a function to update that value (setNavActive).
-    const [NavActive, setNavActive] = useState(false);        
+function Navbar() {
+  //useState is a hook to manage the state of variable 'NavActive'.
+  //The useState hook returns an array with two elements: the current state value (NavActive) and a function to update that value (setNavActive).
+  const [NavActive, setNavActive] = useState(false);
 
-    const toggleNav = () => {
-        setNavActive(!NavActive)
-    }
-    const closeMenu = () => {
-        setNavActive(false)
-    }
-    useEffect(()=>{
-
-    })
+  const toggleNav = () => {
+    setNavActive(!NavActive);
+  };
+  const closeMenu = () => {
+    setNavActive(false);
+  };
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 500) {
+        closeMenu;
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  useEffect(()=>{
+    if(window.innerWidth <=1200){
+        closeMenu;
+    } 
+  },[]);
+  return(
+    <nav className={`navbar ${NavActive ? "active": ""}`}>
+        <div>
+            <img src="./img/logo.svg" alt="Logoipsum"/>
+        </div>
+        <a className={`nav__hamburger ${NavActive ? "active": ""}`} onClick={toggleNav}>
+            <span className="nav__hamburger__line"></span>
+            <span className="nav__hamburger__line"></span>
+            <span className="nav__hamburger__line"></span>
+        </a>
+        <div className={`navbar--items ${NavActive ? "active" : ""}`}></div>
+    </nav>
+  );
 }
+
+export default Navbar;
