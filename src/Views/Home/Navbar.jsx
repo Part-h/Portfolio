5f2ca7f9d7e84836d16768eaacf48c13a283d346
Parth Patel <67832224+Part-h@users.noreply.github.com>
2024-02-29 01:19:45 -0500

    navbar design complete

--- src/Views/Home/Navbar.jsx
+++ src/Views/Home/Navbar.jsx
@@ -2,16 +2,16 @@ import { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 
 function Navbar() {
-  //useState is a hook to manage the state of variable 'NavActive'.
-  //The useState hook returns an array with two elements: the current state value (NavActive) and a function to update that value (setNavActive).
-  const [NavActive, setNavActive] = useState(false);
+  const [navActive, setNavActive] = useState(false);
 
   const toggleNav = () => {
-    setNavActive(!NavActive);
+    setNavActive(!navActive);
   };
+
   const closeMenu = () => {
     setNavActive(false);
   };
+
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth <= 500) {
@@ -19,26 +19,103 @@ function Navbar() {
       }
     };
     window.addEventListener("resize", handleResize);
+
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
-  useEffect(()=>{
-    if(window.innerWidth <=1200){
-        closeMenu;
-    } 
-  },[]);
-  return(
-    <nav className={`navbar ${NavActive ? "active": ""}`}>
-        <div>
-            <img src="./img/logo.svg" alt="Logoipsum"/>
-        </div>
-        <a className={`nav__hamburger ${NavActive ? "active": ""}`} onClick={toggleNav}>
-            <span className="nav__hamburger__line"></span>
-            <span className="nav__hamburger__line"></span>
-            <span className="nav__hamburger__line"></span>
-        </a>
-        <div className={`navbar--items ${NavActive ? "active" : ""}`}></div>
+
+  useEffect(() => {
+    if (window.innerWidth <= 1200) {
+      closeMenu;
+    }
+  }, []);
+
+  return (
+    <nav className={`navbar ${navActive ? "active" : ""}`}>
+      <div>
+        <img src="./img/logo.svg" alt="Logoipsum" />
+      </div>
+      <a
+        className={`nav__hamburger ${navActive ? "active" : ""}`}
+        onClick={toggleNav}
+      >
+        <span className="nav__hamburger__line"></span>
+        <span className="nav__hamburger__line"></span>
+        <span className="nav__hamburger__line"></span>
+      </a>
+      <div className={`navbar--items ${navActive ? "active" : ""}`}>
+        <ul>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="heroSection"
+              className="navbar--content"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="MyPortfolio"
+              className="navbar--content"
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="AboutMe"
+              className="navbar--content"
+            >
+              About Me
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="testimonial"
+              className="navbar--content"
+            >
+              Testimonials
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <Link
+        onClick={closeMenu}
+        activeClass="navbar--active-content"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+        to="Contact"
+        className="btn btn-outline-primary"
+      >
+        Contact Me
+      </Link>
     </nav>
   );
 }
