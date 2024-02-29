9c71583930655777d292394c7ff01d1de1549655
Parth Patel <67832224+Part-h@users.noreply.github.com>
2024-02-29 14:26:20 -0500

    about me page complete

---
+++ src/Views/Home/AboutMe.jsx
@@ -0,0 +1,25 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/profile-pic.png" alt="About me" />
+      </div>
+      <div className="hero--section--content--boxabout--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills--section--heading">About Me</h1>
+          <p className="hero--section-description">
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+            repellat optio culpa cumque deserunt quam corrupti. Vero autem
+            repellat quisquam, animi inventore temporibus nam, ullam sit natus
+            tempore, veritatis laborum Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Aut, voluptate recusandae repellendus dolorum quas
+            pariatur dignissimos suscipit perspiciatis deserunt, est rem.
+            Repudiandae esse, rem cum sed distinctio sequi delectus adipisci.{" "}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/Views/Home/HomeScreen/index.jsx
+++ src/Views/Home/HomeScreen/index.jsx
@@ -1,9 +1,13 @@
 import HeroSection from "../HeroSection";
+import MySkills from "../MySkills";
+import AboutMe from "../AboutMe";
 
 export default function Home(){
     return(
         <>                              {/* fragments */}
             <HeroSection/>
+            <MySkills/>
+            <AboutMe/>
         </>
     )
 }
