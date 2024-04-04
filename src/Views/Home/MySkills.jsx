b80254921eecd17025fd8bcb141bfff85e26de47
Parth Patel <67832224+Part-h@users.noreply.github.com>
2024-04-04 18:20:47 -0400

    skills completed

--- src/Views/Home/MySkills.jsx
+++ src/Views/Home/MySkills.jsx
@@ -10,19 +10,6 @@ export default function MySkills() {
             <p> Elevating Experiences, Empowering Innovation: Bringing Skills to Life in Every Pixel.</p>
         </div>
 
-      {/* <div className="skills--section--container">
-        {data?.skills?.map((item, index) => (
-          <div key={index} className="skills--section--card">
-            <div className="skills--section--img">
-              <img src={item.src} alt="Product Chain" />
-            </div>
-            <div className="skills--section--card--content">
-              <h3 className="skills--section--title">{item.title}</h3>
-              <p className="skills--section--description">{item.description}</p>
-            </div>
-          </div>
-        ))}
-      </div> */}
       <div class="skills--section--layer2">
         {data?.skills?.map((item, index) => (
           <div key={index} class="skills--section--card">
@@ -37,13 +24,14 @@ export default function MySkills() {
                 </p>
               </div>
             </div>
+
             <div className="content">
               <div className="backside">
               <FontAwesomeIcon className="myskills--react" icon={faReact} size="lg" style={{color:"#74C0FC",}}/>
-
               </div>
             </div>
           </div>
+          
         ))}
       </div>
     </section>
